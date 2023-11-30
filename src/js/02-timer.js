ad21aed91af5763df53676c630e0d14486a05de6
Albina Vivdenko <albinka3322@gmail.com>
2023-11-30 21:13:10 +0100

    ефіл2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,5 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-// all modules
 import Notiflix from 'notiflix';
 
 
@@ -53,20 +52,13 @@ function convertMs(ms) {
     const hour = minute * 60;
     const day = hour * 24;
 
-    // Remaining days
     const days = addInterface(Math.floor(ms / day));
-    // Remaining hours
     const hours = addInterface(Math.floor((ms % day) / hour));
-    // Remaining minutes
     const minutes = addInterface(Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
     const seconds = addInterface(Math.floor((((ms % day) % hour) % minute) / second));
 
     return { days, hours, minutes, seconds };
 }
-
-
-
 // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
@@ -83,7 +75,7 @@ const timer = {
 
             if (deltaTime <= 1000) {
                 timer.stop();
-                Notiflix.Notify.success(`Congratulations! The timer has stopped!`);
+                Notiflix.Notify.success(`Congratulations!`);
             }
         }, 1000);
     },
