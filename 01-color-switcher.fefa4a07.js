1fc4902b9ec729a3d497535a8d498a0eba1f47c3
AlbinaViv <133263182+AlbinaViv@users.noreply.github.com>
2024-06-14 20:42:00 +0000

    Deploying to gh-pages from @ AlbinaViv/goit-js-hw-09@398c66758e8df31e054c9a7a9a91735bfe88cdf9 🚀

--- 01-color-switcher.fefa4a07.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");document.querySelector("body");let d=null;e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,t.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.fefa4a07.js.map
